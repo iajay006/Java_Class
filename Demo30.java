78b4047c3dc398b8c1db03623e639584084f16fb
Ajay Rana <ajay07503@gmail.com>
2026-01-18 21:51:41 +0530

    class practice

---
+++ Demo30.java
@@ -0,0 +1,12 @@
+class Demo30
+{
+   public static void main(String a[])
+    {
+      int arr[]={7,8,9};
+      
+      System.out.println(arr[0]);
+       System.out.println(arr[1]);
+       System.out.println(arr[2]);
+
+    }
+}
